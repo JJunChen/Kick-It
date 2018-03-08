1925bbdcf96406a38a9e9e5b939be0ecd99a7198
Jiajun Chen <jchen0318@gmail.com>
2018-03-08 15:00:04 -0800

    fixing heroku config var 03

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBarContainer from './components/SearchBarContainer';
-import EventListContainer from './components/EventListContainer';
+import SearchBarContainer from './components/SearchBarContainer.jsx';
+import EventListContainer from './components/EventListContainer.jsx';
 // import WeekendListContainer from './components/WeekendListContainer.jsx';
 
 class App extends Component {
