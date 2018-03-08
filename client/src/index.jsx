701d269b2c32d8ef93a339bd8ae7430431b08e05
Jiajun Chen <jchen0318@gmail.com>
2018-03-08 14:56:30 -0800

    fixing heroku config var 02

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,78 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBarContainer from './components/SearchBarContainer.jsx';
-import EventListContainer from './components/EventListContainer.jsx';
-//import WeekendListContainer from './components/WeekendListContainer.jsx';
+import SearchBarContainer from './components/SearchBarContainer';
+import EventListContainer from './components/EventListContainer';
+// import WeekendListContainer from './components/WeekendListContainer.jsx';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			featured: [],
-			weekend: [],
-		}
-	}
-	componentDidMount() {
-		fetch('/initialLoad')
-			.then((response) =>{
-				return response.json();
-			})
-			.then((data) =>{
-				this.setState({
-					featured: data.today,
-				});
-			})
-		.then(()=>{
-			fetch('/weekend')
-			.then((response) =>{
-				return response.json();
-			})
-			.then((data) =>{
-				let events = JSON.parse(data).events;
-				this.setState({
-					weekend: events,
-				});
-			})
-		})
-	}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      featured: [],
+      weekend: [],
+    };
+    this.runFilters = this.runFilters.bind(this);
+  }
+  componentDidMount() {
+    fetch('/initialLoad')
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          featured: data.today,
+        });
+      })
+      .then(() => {
+        fetch('/weekend')
+          .then(response => response.json())
+          .then((data) => {
+            const { events } = JSON.parse(data);
+            this.setState({
+              weekend: events,
+            });
+          });
+      });
+  }
 
-	runFilters(filters) {
-		fetch('/filter', {
-			headers: {
-				//'Accept': 'application/json',
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-			body: JSON.stringify(filters),
-		})
-		.then((response)=> {
+  runFilters(filters) {
+    fetch('/filter', {
+      headers: {
+        // 'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(filters),
+    })
+      .then(response => response.json())
+      .then((events) => {
+        this.setState({
+          featured: events.rows,
+        });
+      });
+  }
 
-			return response.json();
-		})
-		.then((events)=> {
-			this.setState({
-				featured: events.rows
-			})
-		})
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>Kick It</h1>
-				<SearchBarContainer runFilters={this.runFilters.bind(this)}/>
-				<div className="album text-muted">
-					<div className="container">
-						<EventListContainer 
-							featuredEvents={this.state.featured}
-							weekendEvents={this.state.weekend.slice(0,10)} 
-						/>
-					</div>
-				</div>
-				
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>Kick It</h1>
+        <SearchBarContainer runFilters={this.runFilters} />
+        <div className="album text-muted">
+          <div className="container">
+            <EventListContainer
+              featuredEvents={this.state.featured}
+              weekendEvents={this.state.weekend.slice(0, 10)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const https = require("https");
+const https = require('https');
 const getEvents = require('../lib/eventbrite.js');
 const Promise = require('bluebird');
-const PORT = process.env.PORT || 3000;
 const moment = require('moment');
+
+const PORT = process.env.PORT || 3000;
 const { API_key } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(`${__dirname}/../client/dist`));
 
-//======================================================================
-//        Database Functions     
-//======================================================================
+//= =====================================================================
+//        Database Functions
+//= =====================================================================
 const db = require('../database/index.js');
 
 // ======================================================================
@@ -23,59 +24,56 @@ const db = require('../database/index.js');
 //   API weekend's events ->  Client
 // ======================================================================
 
-app.get('/initialLoad', function (req, res) {
-  let responseObj = {};
+app.get('/initialLoad', (req, res) => {
+  const responseObj = {};
   let holder = [];
-  let start = moment().format();
-  let end = moment().add(30,'day').format();
-  console.log(end);
+  const start = moment().format();
+  const end = moment().add(30, 'day').format();
 
-  let month_options = { 
-      method: 'GET',
-      url: 'https://www.eventbriteapi.com/v3/events/search',
-      qs: 
+  const month_options = {
+    method: 'GET',
+    url: 'https://www.eventbriteapi.com/v3/events/search',
+    qs:
       {
-          //'start_date.keyword': 'this_month',
-          'start_date.range_start': '2017-11-27T19:00:00',
-          'start_date.range_end': '2017-12-30T19:00:00',
-          'location.address' : 'san francisco',
-          'categories' : '103,110,113,116,17001,104,105,102,118,108,109',
-          'page' : 1,
+        // 'start_date.keyword': 'this_month',
+        'start_date.range_start': '2017-11-27T19:00:00',
+        'start_date.range_end': '2017-12-30T19:00:00',
+        'location.address': 'san francisco',
+        categories: '103,110,113,116,17001,104,105,102,118,108,109',
+        page: 1,
       },
-    headers: 
-    { authorization: API_key }, 
-  }
-  
-  let getCalls = () =>{
-    return new Promise((resolve, reject) =>{ 
-      request(month_options, function(error, response, body){
-        let page = JSON.parse(body).pagination.page_number;
-        let parsedEvents = JSON.parse(body).events;
-        if (!error) {
-            holder = holder.concat(parsedEvents);
-          if (page < 5) {
-            month_options.qs.page +=1;
-            resolve(getCalls());
-          } else {
-            resolve(holder);
-          }
+    headers:
+    { authorization: API_key },
+  };
+
+  const getCalls = () => new Promise((resolve, reject) => {
+    request(month_options, (error, response, body) => {
+      const page = JSON.parse(body).pagination.page_number;
+      const parsedEvents = JSON.parse(body).events;
+      if (!error) {
+        holder = holder.concat(parsedEvents);
+        if (page < 5) {
+          month_options.qs.page += 1;
+          resolve(getCalls());
         } else {
-          console.log(error);
-          reject(error)
+          resolve(holder);
         }
-      });
-    })
-  }
+      } else {
+        console.log(error);
+        reject(error);
+      }
+    });
+  });
 
   getCalls()
-  .then((holder)=>{
-    console.log(holder.length);
-    return holder.map((event) => {
-        let imageUrl = event.logo ? event.logo.url : 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png';
-        let catID = event.subcategory_id === 17001 ? event.subcategory_id : event.category_id; 
-        let defaultPrice = event.is_free ? 'free' : 'paid';
-        let eventName = `$$${event.name.text}$$`;
-        let eventDesc = `$$${event.description.text}$$`;
+    .then((holder) => {
+      console.log(holder.length);
+      return holder.map((event) => {
+        const imageUrl = event.logo ? event.logo.url : 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png';
+        const catID = event.subcategory_id === 17001 ? event.subcategory_id : event.category_id;
+        const defaultPrice = event.is_free ? 'free' : 'paid';
+        const eventName = `$$${event.name.text}$$`;
+        const eventDesc = `$$${event.description.text}$$`;
         return {
           id: event.id,
           name: eventName,
@@ -88,70 +86,70 @@ app.get('/initialLoad', function (req, res) {
           end_datetime: event.end.local,
           category_id: catID,
           day: moment(event.start.local).format('dddd'),
-        }
-      })
+        };
+      });
     })
     .then((formattedEvents) => {
       // ADD TO DB
       db.addEvents(formattedEvents)
-        .then( (results) => { 
-      // //GET TODAYS EVENTS FROM THE DB
+        .then((results) => {
+          // //GET TODAYS EVENTS FROM THE DB
           db.getTodaysEvents()
-            .then((data) =>{
+            .then((data) => {
               responseObj.today = data.rows;
               res.json(responseObj);
-            })
-        })
+            });
+        });
     })
-    .then(()=>{
-      app.get('/weekend', function(req, res) {
-      getEvents.weekend()
-      .then((data) =>{
-        res.json(data);
-      })
-    })
-  });
+    .then(() => {
+      app.get('/weekend', (req, res) => {
+        getEvents.weekend()
+          .then((data) => {
+            res.json(data);
+          });
+      });
+    });
 });
 
 // ======================================================================
 //                    Query the DB on client filters
 // ======================================================================
-app.post('/filter', function(req,res) {
-  let date = req.body.date;
-  let categories = req.body.category;
-  let price = req.body.price;
+app.post('/filter', (req, res) => {
+  const date = req.body.date;
+  const categories = req.body.category;
+  const price = req.body.price;
 
   db.searchAllEvents(date, categories, price)
     .then((data) => {
       res.json(data);
-    })
+    });
 });
 
 
 // ======================================================================
 //                    Send today's data back to the client
 // ======================================================================
-app.get('/loadToday', function (req, res) {
+app.get('/loadToday', (req, res) => {
   getEvents.today()
-    .then((data) =>{
+    .then((data) => {
       res.json(data);
     });
-  //getTodayEventsDB
+  // getTodayEventsDB
 });
 // ======================================================================
 //                    load Venues to DB
 // ======================================================================
-app.get('/loadVenues', function (req, res) {
+app.get('/loadVenues', (req, res) => {
   getEvents.month()
-    .then((data) =>{
+    .then((data) => {
       res.json(data);
     });
-}); 
+});
 
 // ======================================================================
 //                    Run Server
 // ======================================================================
 
-module.exports = app.listen(PORT, function() {
+module.exports = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
 });
